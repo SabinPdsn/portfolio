ab7673fefd78094e757e9c5a906ecceac15be315
SabinPdsn <sbnpdsn@gmail.com>
2025-12-19 12:32:15 +0545

    update Contact component title and downgrade TypeScript version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,12 +4,8 @@ function Contact() {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-12">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            Contact Us
+            Contact Me
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-            gentrify.
-          </p>
         </div>
         <form action="https://getform.io/f/bkkypykb" method="POST" className="lg:w-1/2 md:w-2/3 mx-auto">
           <div className="flex flex-wrap -m-2">
